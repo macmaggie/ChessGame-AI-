1bda38ffde686f9ffaee2294369c5673017960f5
Macmagdalena <17U-MMaciak@cap.education>
2021-04-28 09:31:59 +0100

    menu

--- Chess ai design/Chess ai design/Menu.cs
+++ Chess ai design/Chess ai design/Menu.cs
@@ -22,18 +22,25 @@ namespace Chess_ai_design
             string Contents = "1WC1WH1WB1WQ1WK1WB1WH1WC:8wP:8--:8--:8--:8--:8BP:1BC1BH1BB1BQ1BK1BB1BH1BC";
             var tableform = new TableForm();
             tableform.Show();
-            tableform.AddButtons(Contents);
+            tableform.AddButtons(Contents);//loads the chesspieces and buttons
             //open Table form and close the previous form
-            //load from the new game code 
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
+            string Contents = null;
             //Open Select form to open a note file and close the previous form
             OpenFileDialog PickFile = new OpenFileDialog();
             PickFile.Filter = "Text File|*.txt";
             if (PickFile.ShowDialog() == System.Windows.Forms.DialogResult.OK) // checks if they have chosen and opened a file 
-            { }
+            { 
+                //read the file choosen, put contents into contents string
+                //open the tableform and send the Contents through
+                var tableform = new TableForm();
+                tableform.Show();
+                tableform.AddButtons(Contents);//loads the chesspieces and buttons
+            }
+            
             //open table form close select form 
             //Load game from load game code
         }
