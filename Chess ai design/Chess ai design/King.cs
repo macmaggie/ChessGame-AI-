37a42ac5a79d1773cc1876b31a0705e65d9f510d
macmaggie <macmagdalena@yahoo.co.uk>
2021-05-19 19:33:58 +0100

    Kiings moves

--- Chess ai design/Chess ai design/King.cs
+++ Chess ai design/Chess ai design/King.cs
@@ -39,7 +39,38 @@ namespace Chess_ai_design
         }
         public override void ColorButton(ref int x, ref int y, ref Button[,] ChessBoard, ref ChessPiece[,] ChessPieces)
         {
-
+            if (x >= 1 && y >= 1 && (ChessPieces[x - 1, y - 1] == null || ChessPieces[x -1, y - 1].GetData() != Colour))
+            {
+                ChessBoard[x - 1, y - 1].BackColor = Color.Red;
+            }
+            if (x >= 0 && y >= 1 && (ChessPieces[x, y - 1] == null || ChessPieces[x, y - 1].GetData() != Colour))
+            {
+                ChessBoard[x, y - 1].BackColor = Color.Red;
+            }
+            if (x >= 1 && y >= 0 && (ChessPieces[x -1, y] == null || ChessPieces[x -1, y].GetData() != Colour))
+            {
+                ChessBoard[x - 1, y].BackColor = Color.Red;
+            }
+            if (x <= 6 && y <= 6 && (ChessPieces[x + 1, y + 1] == null || ChessPieces[x + 1, y + 1].GetData() != Colour))
+            {
+                ChessBoard[x + 1, y + 1].BackColor = Color.Red;
+            }
+            if (x <= 7 && y <= 6 && (ChessPieces[x, y + 1] == null || ChessPieces[x, y + 1].GetData() != Colour))
+            {
+                ChessBoard[x, y + 1].BackColor = Color.Red;
+            }
+            if (x <= 6 && y <= 7 && (ChessPieces[x + 1, y] == null || ChessPieces[x + 1, y].GetData() != Colour))
+            {
+                ChessBoard[x + 1, y].BackColor = Color.Red;
+            }
+            if (x >= 1 && y <= 6 && (ChessPieces[x -1, y + 1] == null || ChessPieces[x -1, y + 1].GetData() != Colour))
+            {
+                ChessBoard[x - 1, y + 1].BackColor = Color.Red;
+            }
+            if (x <= 6 && y >= 1 && (ChessPieces[x + 1, y - 1] == null || ChessPieces[x + 1, y - 1].GetData() != Colour))
+            {
+                ChessBoard[x + 1, y - 1].BackColor = Color.Red;
+            }
         }
 
     }
